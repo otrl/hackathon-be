5052c23e1b1e0af846feba7a2a14f0fb194c268d
Lefteris Kokkonas <eleftherios.kokkonas@ontrackretail.co.uk>
2018-07-19 13:36:36 +0100

    Amends

--- src/controllers/WorkHomeCatchmentController.ts
+++ src/controllers/WorkHomeCatchmentController.ts
@@ -4,39 +4,38 @@ import db from "../db";
 
 export default class WorkHomeCatchmentController {
     static async post(req: Request, res: Response) {
-        const {postcode, type} = req.body;
+        const {postCode, type} = req.body;
 
         const where: any = {};
 
         if (type === "work") {
             where.work_postal_sector = {
-                [db.sequelize.Op.like]: `${postcode}%`
+                [db.sequelize.Op.like]: `${postCode}%`
             };
         } else {
             where.home_postal_sector = {
-                [db.sequelize.Op.like]: `${postcode}%`
+                [db.sequelize.Op.like]: `${postCode}%`
             };
         }
-
-        console.log(db);
 
         try {
             const results = await db.workershomecatchment.findAll({ where });
 
             console.log(results);
+            return res.json({data: results});
 
-            return res.json({data: [
-                    {
-                        work_postal_sector: "SE18 6",
-                        home_postal_sector: "E7 8",
-                        users: 10,
-                    },
-                    {
-                        work_postal_sector: "SE18",
-                        home_postal_sector: "NW1 1",
-                        users: 62,
-                    }
-                ]});
+            // return res.json({data: [
+            //         {
+            //             work_postal_sector: "SE18 6",
+            //             home_postal_sector: "E7 8",
+            //             users: 10,
+            //         },
+            //         {
+            //             work_postal_sector: "SE18",
+            //             home_postal_sector: "NW1 1",
+            //             users: 62,
+            //         }
+            //     ]});
         } catch (err) {
             return res.status(500).json({err: err.message});
         }
